cb353bb176c16b86f444428fa3eca754d793bc5e
OFAxTAB <rockenny@ymail.com>
2023-09-03 11:41:22 +0300

    add viewset to course model add generics to lesson model

--- lms/models.py
+++ lms/models.py
@@ -9,9 +9,23 @@ class Course(models.Model):
     description = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
 
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
 
 class Lesson(models.Model):
     title = models.CharField(max_length=100, verbose_name='название')
     description = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
     video_link = models.CharField(max_length=200, verbose_name='видео_ссылка')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
---
+++ lms/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from lms import views
+from lms.apps import LmsConfig
+
+app_name = LmsConfig.name
+
+router = DefaultRouter()
+router.register(r'course', views.CourseViewSet, basename='courses')
+
+
+urlpatterns = [
+    path('lesson/create/', views.LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', views.LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', views.LessonRetrieveAPIView.as_view(), name='lesson-get'),
+    path('lesson/update/<int:pk>/', views.LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', views.LessonCreateAPIView.as_view(), name='lesson-delete'),
+] + router.urls
+
+
+
+
+
