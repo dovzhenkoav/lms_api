eaababbf05a6cc3bbb4a420e911bb69abf721f68
OFAxTAB <rockenny@ymail.com>
2023-09-04 21:53:30 +0300

    add nested lessons in course fix lessons count in course

--- lms/serializers.py
+++ lms/serializers.py
@@ -10,14 +10,16 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    lessons = serializers.SerializerMethodField()
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source='lesson', many=True)
 
     class Meta:
         model = Course
         fields = '__all__'
 
-    def get_lessons(self, instance):
-        if instance.lesson.all().first():
-            return instance.lesson.all().first().title
+    def get_lessons_count(self, instance):
+        course_lessons = instance.lesson.all()
+        if course_lessons:
+            return len(course_lessons)
         return 0
 
